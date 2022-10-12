c7cde68561935e42749280933388f9612fe05c2b
santiago.hurtado314@gmail.com <santiago.hurtado314@gmail.com>
2022-10-12 16:02:10 -0400

    set player name

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -88,7 +88,7 @@ var randomNumber = function(min, max) {
       if (playerInfo.health > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-  
+        
         // pick new enemy to fight based on the index of the enemyInfo array
         var pickedEnemyObj = enemyInfo[i];
   
@@ -178,12 +178,25 @@ var randomNumber = function(min, max) {
   };
   
   /* END GAME FUNCTIONS */
+
+  //function to set name to reject null or blanc as names 
+  var getPlayerName = function() {
+    
+    var name = "";
+
+    while (name === "" || name === null) {
+      name = prompt("what is your robot's name?");
+    }
+
+    console.log("Your robot's name is" + name);
+    return name;
+  }
   
   /* GAME INFORMATION / VARIABLES */
   
   // player information
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
